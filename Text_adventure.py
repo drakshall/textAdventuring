df899d8eab1dfe62069669fcb23b31a90b8d160b
harveylloyd487 <149804061+harveylloyd487@users.noreply.github.com>
2024-03-14 13:48:22 +0000

    Update Text_adventure.py

--- Text_adventure.py
+++ Text_adventure.py
@@ -1,1 +1,12 @@
+import unittest
+import io
+import contextlib
+import sys
+from unittest.mock import Mock
+ 
+from textadventure import *
 
+class HarveyRoomTest(unittest.TestCases):
+  def has_room_been_created(self)
+    room = HarveyRoom(none)
+  
